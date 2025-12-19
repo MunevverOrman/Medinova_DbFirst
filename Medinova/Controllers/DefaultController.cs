d4abff227a5825a0b959303526ba52e68e055ec1
Münevver Orman <munevverorman1@gmail.com>
2025-12-19 22:19:17 +0300

    Add project files.

---
+++ Medinova/Controllers/DefaultController.cs
@@ -0,0 +1,99 @@
+﻿using Medinova.Dtos;
+using Medinova.Enums;
+using Medinova.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Medinova.Controllers
+{
+    public class DefaultController : Controller
+    {
+        MedinovaContext context = new MedinovaContext();
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult DefaultAppointment()
+        {
+            var departments = context.Departments.ToList();
+            ViewBag.Departments = (from department in departments
+                                   select new SelectListItem
+                                   {
+                                       Text = department.Name,
+                                       Value = department.DepartmentId.ToString()
+                                   }).ToList();
+            var dateList=new List<SelectListItem>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                var date = DateTime.Now.AddDays(i);
+                dateList.Add(new SelectListItem
+                {
+                    Text = date.ToString("dd.MMMM.dddd"),
+                    Value = date.ToString("yyyy-MM-dd")
+                });
+            }
+            ViewBag.Dates = dateList;
+
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult MakeAppointment(Appointment appointment) 
+        { 
+            
+            appointment.IsActive= true;
+            context.Appointments.Add(appointment);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public JsonResult GetDoctorsByDepartmentId(int departmentId)
+        {
+            var doctors = context.Doctors.Where(x => x.DepartmentId == departmentId)
+                                          .Select(doctor => new SelectListItem
+                                          {
+                                              Text = doctor.FullName,
+                                              Value = doctor.DoctorId.ToString()
+                                          }).ToList();
+
+            return Json(doctors, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GetAvailableHours(DateTime selectedDate, int doctorId)
+        {
+            var bookedTimes = context.Appointments
+                .Where(x => x.DoctorId == doctorId && x.AppointmentDate == selectedDate)
+                .Select(x => x.AppointmentTime)
+                .ToList();
+
+            var dtoList = new List<AppointmentAvailabilityDto>();
+
+            foreach (var hour in Times.AppointmentHours)
+            {
+                var dto = new AppointmentAvailabilityDto();
+                dto.Time = hour;
+
+                if (bookedTimes.Contains(hour))
+                {
+                    dto.IsBooked = true;
+                }
+                else
+                {
+                    dto.IsBooked = false;
+                }
+
+                dtoList.Add(dto);
+            }
+
+            return Json(dtoList, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ Medinova/Dtos/AppointmentAvailabilityDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Medinova.Dtos
+{
+    public class AppointmentAvailabilityDto
+    {
+        public string Time { get; set; }
+
+        public bool IsBooked{ get; set; }
+    }
+}
